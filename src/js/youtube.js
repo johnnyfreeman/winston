d10f5cf325b40381324501abd096635c6af47b05
Johnny Freeman <prsjohnny@gmail.com>
2015-03-18 12:52:05 -0400

    improved youtube package. added new keywords. added concept of hard and soft keywords.

--- src/js/youtube.js
+++ src/js/youtube.js
@@ -1,25 +1,49 @@
 var Youtube = function (searchInput) {
     this.searchInput = searchInput;
+
+    // hard keywords are stripped from the query
+    this.hardKeywords = ['video', 'youtube'];
+    this.softKeywords = ['cover', 'movie', 'music', 'trailer', 'tutorial'];
 };
 
 Youtube.prototype.inputHandler = function () {
     var deferred = Q.defer();
     var input = this.searchInput.value;
-    var query = input.slice(8);
-    var commands = [
-        {
-            query: query,
-            title: "Search for '" + query + "'",
-            description: "YouTube: Open search results for '" + query + "'",
-            icon: 'youtube',
-            action: 'Search',
-            run: function () {
-                chrome.tabs.create({
-                    url: 'https://www.youtube.com/results?search_query=' + this.query
-                });
-            }
+    var commands = [];
+
+    // determine if input contains any keywords
+    var keywordFound = false;
+    this.hardKeywords.concat(this.softKeywords).forEach(function (keyword) {
+        console.log(keyword, input.indexOf(keyword));
+        if (input.indexOf(keyword) > -1) {
+            keywordFound = true;
         }
-    ];
-    deferred.resolve(input.indexOf("youtube") > -1 ? commands : []);
+    });
+
+    var query = input;
+    if (keywordFound) {
+        // stip out hard keywords
+        this.hardKeywords.forEach(function (hardKeyword) {
+            query = query.replace(hardKeyword, '').trim();
+        });
+
+        // add command
+        commands.push(new YoutubeSearchCommand(query));
+    }
+
+    deferred.resolve(commands);
     return deferred.promise;
+};
+
+var YoutubeSearchCommand = function (query) {
+    this.id = 'YOUTUBE1';
+    this.query = query;
+    this.icon = 'youtube';
+    this.action = 'Search';
+    this.title = 'YouTube "' + this.query + '"';
+    this.description = 'YouTube: Open YouTube search results';
+};
+
+YoutubeSearchCommand.prototype.run = function () {
+    chrome.tabs.create({ url: 'https://www.youtube.com/results?search_query=' + this.query });
 };
