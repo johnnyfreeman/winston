22fca5b7aca9b7d608b68b9f286930eb3f69cbf8
Johnny Freeman <prsjohnny@gmail.com>
2015-03-28 14:08:18 -0400

    cleaned up calculator results

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -227,20 +227,22 @@ var Calculator = function (searchInput) {
 Calculator.prototype.inputHandler = function () {
     var commands = [];
     var input = this.searchInput.value;
+    var result;
 
     try {
-        var command = {};
-        command.id = 'CALCULATOR1';
-        command.result = math.eval(input);
-        command.title = command.result.toString();
-        command.description = "Calculator: Copy '" + command.title + "' to your clipboard";
-        command.action = 'Copy';
-        command.icon = 'calculator';
-        command.run = function () {
-            document.execCommand('copy');
-        };
-
-        if (input.length > 0) commands.push(command);
+        result = math.format(math.eval(input), 2);
+        if (result !== 'undefined' && result !== 'function') {
+            commands.push({
+                id: 'CALCULATOR1',
+                title: result,
+                description: "Calculator: Copy '" + result + "' to your clipboard",
+                action: 'Copy',
+                icon: 'calculator',
+                run: function () {
+                    document.execCommand('copy');
+                }
+            });
+        }
     } catch(e) {}
 
     return commands;
